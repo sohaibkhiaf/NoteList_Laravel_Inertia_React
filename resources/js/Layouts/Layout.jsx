8d986c7aa2b53ce18fd9cfa5000ec49975724817
Sohaib Khiaf <sohaibkhiaf@gmail.com>
2025-12-24 14:54:18 +0100

    Home responsive

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,9 +1,13 @@
 import Header from "./Header.jsx";
+import { Head } from "@inertiajs/react";
 
-function Layout ({header, children}){
+function Layout ({ header, children}){
 
     return (
         <>
+            <Head>
+                <link rel="icon" type="image/png" href="note-icon.png" />
+            </Head>
             <Header selected={header ?? "undefined"}/>
 
             <main>
--- resources/js/Layouts/Note.jsx
+++ resources/js/Layouts/Note.jsx
@@ -7,15 +7,23 @@ function Note ({note}){
     return (
         <div className="note-container">
             <h4 className="note-title">{note.title}</h4>
-            <p className="note-body">&nbsp;&nbsp;&nbsp;{note.body}</p>
+            <p className="note-body">{truncateText(note.body)}</p>
             <i className="note-date">{new Date(note.created_at).toLocaleString()}</i>
 
             <Link className="note-link"
                 href={route('notes.show', note)}>
-                    Read more...
+                    Read more
             </Link>
         </div>
     );
 }
+
+function truncateText(text, maxLength = 230) {
+    if (text.length <= maxLength) {
+        return text; // no need to truncate
+    }
+    return text.slice(0, maxLength) + '...';
+}
+
 
 export default Note;
